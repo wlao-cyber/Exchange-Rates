5f4ceadc07c9fef37096b485f3e8005a59b503f7
Wilson Lao <68834234+wlao-cyber@users.noreply.github.com>
2023-01-09 01:27:18 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// import { euro_rate, cad_rate, hkd_rate, yen_rate, peso_rate } from "./rates.js";
+
+import {autoload} from "./autoload.js"
+import { calculations } from "./calculations.js";
+import {reset } from "./reset.js"
+
+// autoload();
+// calculations();
+
+//  modules will have its own scope. They are not available in the global scope like the normal scripts.
+window.autoload = autoload;
+window.calculations = calculations;
+window.reset = reset;
+
+// autoload();
+// calculations();
---
+++ js/reset.js
@@ -0,0 +1,9 @@
+export function reset()
+{
+    document.getElementById("results_euro").value = "";
+    document.getElementById("results_cad").value = "";
+    document.getElementById("results_hkd").value = "";
+    document.getElementById("results_yen").value = "";
+    document.getElementById("results_peso").value = "";
+    document.getElementById("usd_amount").value = "";
+}
