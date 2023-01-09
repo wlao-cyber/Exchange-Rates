75c74a05fd14e91b4a68ebeb5d35b315ceec4a1e
Wilson Lao <68834234+wlao-cyber@users.noreply.github.com>
2023-01-09 11:16:17 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// import { euro_rate, cad_rate, hkd_rate, yen_rate, peso_rate } from "/rates.js";
+// import { euro_rate, cad_rate, hkd_rate, yen_rate, peso_rate } from "https://github.com/wlao-cyber/Exchange-Rates/blob/main/js/rates.js";
 
 import {autoload} from "/autoload.js"
 import { calculations } from "/calculations.js";
