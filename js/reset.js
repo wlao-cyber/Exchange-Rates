c21146032bbe362e2cab927fd53a701314bd5288
Wilson Lao <68834234+wlao-cyber@users.noreply.github.com>
2023-01-09 11:50:47 -0800

    Delete js directory

--- js/reset.js
+++
@@ -1,9 +0,0 @@
-export function reset()
-{
-    document.getElementById("results_euro").value = "";
-    document.getElementById("results_cad").value = "";
-    document.getElementById("results_hkd").value = "";
-    document.getElementById("results_yen").value = "";
-    document.getElementById("results_peso").value = "";
-    document.getElementById("usd_amount").value = "";
-}
