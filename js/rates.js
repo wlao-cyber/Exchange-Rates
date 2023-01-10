d6ec8e17976876154975d40ca9eac04ae61c937f
Wilson Lao <68834234+wlao-cyber@users.noreply.github.com>
2023-01-10 12:04:24 -0800

    Update rates.js

--- js/rates.js
+++ js/rates.js
@@ -1,9 +1,9 @@
 // the dollar rate for each currency should be constant and not change
 // variables have to be global for calculations() function to use them.
-const euro_rate = .94;
-const cad_rate = 1.36;
-const hkd_rate = 7.82;
-const yen_rate = 130.31;
-const peso_rate = 19.33;
+const euro_rate = .93;
+const cad_rate = 1.34;
+const hkd_rate = 7.81;
+const yen_rate = 132.22;
+const peso_rate = 19.06;
 
 export {euro_rate, cad_rate, hkd_rate, yen_rate, peso_rate};
